9c3db0019798b1937d487105f4f28e1792e2301c
Paul Nguyen <pnguyen6910@gmail.com>
2022-04-11 16:41:56 -0500

    setting up backend

---
+++ social_media_image_backend/schemas/comment.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'comment',
+    title: 'Comment',
+    type: 'document',
+    fields: [
+        {
+            name: 'postedBy',
+            title: 'PostedBy',
+            type: 'PostedBy'
+        },
+        {
+            name: 'comment',
+            title: 'Comment',
+            type: 'string'
+        }
+    ]
+}
---
+++ social_media_image_backend/schemas/save.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'save',
+    title: 'Save',
+    type: 'document',
+    fields: [
+        {
+            name: 'postedBy',
+            title: 'PostedBy',
+            type: 'PostedBy'
+        },
+        {
+            name: 'userId',
+            title: 'UserId',
+            type: 'string'
+        }
+    ]
+}
