3b53df090ff25a461755ad788c09e5c4b000dbd3
Paul Nguyen <pnguyen6910@gmail.com>
2022-04-12 12:53:08 -0500

    setting up client and successful google login

--- social_media_image_backend/schemas/save.js
+++ social_media_image_backend/schemas/save.js
@@ -1,17 +1,17 @@
 export default {
-    name: 'save',
-    title: 'Save',
-    type: 'document',
-    fields: [
-        {
-            name: 'postedBy',
-            title: 'PostedBy',
-            type: 'PostedBy'
-        },
-        {
-            name: 'userId',
-            title: 'UserId',
-            type: 'string'
-        }
-    ]
-}
+  name: "save",
+  title: "Save",
+  type: "document",
+  fields: [
+    {
+      name: "postedBy",
+      title: "PostedBy",
+      type: "PostedBy",
+    },
+    {
+      name: "userId",
+      title: "UserId",
+      type: "string",
+    },
+  ],
+};
--- social_media_image_frontend/src/components/Login.jsx
+++ social_media_image_frontend/src/components/Login.jsx
@@ -4,8 +4,22 @@ import { useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 import shareVideo from "../assets/share.mp4";
 import logo from "../assets/logowhite.png";
+import client from "../client";
 
 const Login = () => {
+  const responseGoogle = (response) => {
+    console.log(response);
+    localStorage.setItem("user", JSON.stringify(response));
+
+    const { name, googleId, imageUrl } = response.profileObj;
+    const doc = {
+      _id: googleId,
+      _type: "user",
+      userName: name,
+      image: imageUrl,
+    };
+  };
+
   return (
     <div className="flex justify-start items-center flex-col h-screen">
       <div className="relative w-full h-full">
@@ -22,6 +36,25 @@ const Login = () => {
         <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay">
           <div className="p-5">
             <img src={logo} width="130px" alt="logo" />
+          </div>
+
+          <div className="shadow-2x1">
+            <GoogleLogin
+              clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
+              render={(renderProps) => (
+                <button
+                  type="button"
+                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  <FcGoogle className="mr-4" /> Sign in with Google
+                </button>
+              )}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy="single_host_origin"
+            />
           </div>
         </div>
       </div>
