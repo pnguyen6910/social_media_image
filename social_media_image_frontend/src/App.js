240cbd534ca48350d2df07db9b66805a84964820
Paul Nguyen <pnguyen6910@gmail.com>
2022-04-11 19:21:21 -0500

    setting up login & home

--- social_media_image_frontend/src/App.js
+++ social_media_image_frontend/src/App.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import Login from '../components/Login'
+import Home from '../container/Home'
 
 const App = () => {
   return (
-    <div>HelloWorld</div>
+    <Routes>
+        <Route path="login" element={ <Login /> } />
+        <Route path="/*" element={ <Home /> } />
+    </Routes>
   )
-}
+};
 
-export default App
+export default App;
