bc98feee95fa5cb2d1d421c1ddc46c47b87c232a
Paul Nguyen <pnguyen6910@gmail.com>
2022-04-14 17:08:58 -0500

    adding container for pins

--- social_media_image_frontend/src/components/Login.jsx
+++ social_media_image_frontend/src/components/Login.jsx
@@ -4,9 +4,10 @@ import { useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 import shareVideo from "../assets/share.mp4";
 import logo from "../assets/logowhite.png";
-import client from "../client";
+import { client } from "../client";
 
 const Login = () => {
+  const navigate = useNavigate();
   const responseGoogle = (response) => {
     console.log(response);
     localStorage.setItem("user", JSON.stringify(response));
@@ -18,6 +19,10 @@ const Login = () => {
       userName: name,
       image: imageUrl,
     };
+
+    client.createIfNotExists(doc).then(() => {
+      navigate("/", { replace: true });
+    });
   };
 
   return (
